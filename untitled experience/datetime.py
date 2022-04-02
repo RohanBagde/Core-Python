29fe39ea5782f3f897d5343b0be51bd3f9ff3166
RohanBagde <93389830+RohanBagde@users.noreply.github.com>
2022-04-02 13:50:04 +0530

    Add files via upload

---
+++ untitled experience/datetime.py
@@ -0,0 +1,16 @@
+#datetime.py
+def now():
+	y=int(input("e s n:"))
+	m=int(input("e s n:"))
+	d=int(input("e s n:"))
+	H=int(input("e s n:"))
+	M=int(input("e s n:"))
+	s=int(input("e s n:"))
+
+
+
+
+#import datetime
+#now=datetime.datetime.now()
+#print("current date and time is:")
+#print(now.strftime("%y-%m-%d %H:%M:%s"))
---
+++ untitled experience/datetimereal.py
@@ -0,0 +1,5 @@
+#datetimereal.py
+import datetime
+now=datetime.now()
+print("current date and time is:")
+print(now."%y-%m-%d %H:%M:%s")
